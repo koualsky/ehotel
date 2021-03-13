6dd59e9ecfe5e9bb459b7d3fe5acb8ac9003e6de
koualsky <koualsky@gmail.com>
2021-03-13 23:58:56 +0100

    Added models and migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+ROOM_CLASSES = {'A': 200, 'B': 150, 'C': 100, 'D': 50}
+
+
+class Room(models.Model):
+    room_number = models.PositiveIntegerField(unique=True)
+    room_class = models.CharField(
+        max_length=32,
+        choices=[item for item in ROOM_CLASSES.items()],
+        default=ROOM_CLASSES['A'],
+    )
+
+    def __str__(self):
+        return str(self.room_number)
+
+
+class Booking(models.Model):
+    rooms = models.ManyToManyField(Room)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    reservation_from = models.DateTimeField()
+    reservation_to = models.DateTimeField()
+
+    def __str__(self):
+        return str("{} {} (from {} to {})".format(
+            self.first_name,
+            self.last_name,
+            self.reservation_from,
+            self.reservation_to
+        ))
