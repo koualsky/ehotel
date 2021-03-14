3560c266bf96dc41f0582311c647fbc383b5ab96
koualsky <koualsky@gmail.com>
2021-03-14 01:27:31 +0100

    Added api to urls

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+from django.conf.urls import include, url
+from .api import RoomViewSet, BookingViewSet
+
+
+router = DefaultRouter()
+router.register(r'room', RoomViewSet)
+router.register(r'booking', BookingViewSet)
+
+
+urlpatterns = [
+    url(r'^api/', include(router.urls)),
+]
