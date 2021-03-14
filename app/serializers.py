130b5fe7311d59d21eb7d5fc83c86c417035a56b
koualsky <koualsky@gmail.com>
2021-03-14 01:24:12 +0100

    Added serializers for basic models

---
+++ app/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Room, Booking
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    rooms = RoomSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Booking
+        fields = "__all__"
