3c03d133926058a7b6214e311cc339c8e69cd0fb
koualsky <koualsky@gmail.com>
2021-03-15 11:37:37 +0100

    Added 'price' field in Room serializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,11 +1,17 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Room, Booking
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from .models import Room, Booking, ROOM_CLASSES
 
 
 class RoomSerializer(ModelSerializer):
+    price = SerializerMethodField()
+
     class Meta:
         model = Room
-        fields = "__all__"
+        fields = ("id", "room_number", "room_class", "price")
+
+    @staticmethod
+    def get_price(obj):
+        return ROOM_CLASSES[obj.room_class]
 
 
 class BookingSerializer(ModelSerializer):
