3136c8e6811549233f5146e6fd9a975706e27806
koualsky <koualsky@gmail.com>
2021-03-17 00:07:37 +0100

    Fixed cors version

--- ehotel/settings.py
+++ ehotel/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = False
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
